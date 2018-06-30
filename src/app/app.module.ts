84cbc6e9b9c3cbf35b99038be9c3573717fb5082
Jacob <jacob.biederman@mymail.champlain.edu>
2018-06-30 14:08:21 -0500

    Basic setup with dropdown
    installed bootstrap, ngx-bootstrap, and sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {AlertModule} from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectDisplayComponent } from './project/project-display/project-display.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    AboutMeComponent,
+    ProjectComponent,
+    ProjectDisplayComponent,
+    ProjectDetailComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
