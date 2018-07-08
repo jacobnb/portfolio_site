bdcf693807524ee9832a138417a2910cd27908b8
Jacob Biederman <jnbiederman@gmail.com>
2018-07-08 18:52:01 -0500

    swapped to mdbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import {AlertModule} from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
@@ -14,6 +14,9 @@ import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
+const appRoutes: Routes = [
+  { path: 'path', component: AppComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,8 +32,9 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AlertModule.forRoot(),
-    BsDropdownModule.forRoot(),
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
