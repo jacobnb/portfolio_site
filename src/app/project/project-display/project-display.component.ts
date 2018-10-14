1a2c315df147568042b6106f3d1bafce443dde52
Jacob <jacob.biederman@mymail.champlain.edu>
2018-10-14 19:55:06 -0400

    Working on basic portfolio, need to fix data binding

--- src/app/project/project-display/project-display.component.ts
+++ src/app/project/project-display/project-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-display',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-display.component.css']
 })
 export class ProjectDisplayComponent implements OnInit {
+  // Neither of these are working right now
+  @Input() imageSource: string = "default";
+  @Input() description: string = "Default Description";
 
   constructor() { }
 
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ProjectDisplayComponent} from './project-display/project-display.component';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-
+  projects: number[] = [1, 2, 3];//ProjectDisplayComponent[] = [];
+  images: string[] = ["src", "src2", "src3"];
+  descriptions: string[] = ["Desc 1", "Desc 2", "Desc 3"];
+  // The above two should be loaded some better way.
   constructor() { }
+
+  // Load a project display component for each image + description.
+  
 
   ngOnInit() {
   }
